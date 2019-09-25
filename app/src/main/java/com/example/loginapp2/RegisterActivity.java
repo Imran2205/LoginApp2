db6708edf9cc33443e62c55b3fad5cbaac8b987e
imran <imran@Invincible.M>
2019-09-25 15:55:14 +0600

    Initial commit

---
+++ app/src/main/java/com/example/loginapp2/RegisterActivity.java
@@ -0,0 +1,193 @@
+package com.example.loginapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String KEY_STATUS = "status";
+    private static final String KEY_MESSAGE = "message";
+    private static final String KEY_EMAIL = "email";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_EMPTY = "";
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etConfirmPassword;
+    private EditText etEmail;
+    private String username;
+    private String password;
+    private String confirmPassword;
+    private String email;
+    private ProgressDialog pDialog;
+    private String register_url = "http://172.104.112.228/static/phpFiles/register.php";
+    private SessionHandler session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        session = new SessionHandler(getApplicationContext());
+        setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.etUser);
+        etPassword = findViewById(R.id.etPass);
+        etConfirmPassword = findViewById(R.id.etConfirmPass);
+        etEmail = findViewById(R.id.etEmail);
+
+        Button login = findViewById(R.id.btnRegLogin);
+        Button register = findViewById(R.id.btnReg);
+
+        //Launch Login screen when Login Button is clicked
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Retrieve the data entered in the edit texts
+                username = etUsername.getText().toString().toLowerCase().trim();
+                password = etPassword.getText().toString().trim();
+                confirmPassword = etConfirmPassword.getText().toString().trim();
+                email = etEmail.getText().toString().trim();
+                if (validateInputs()) {
+                    registerUser();
+                }
+
+            }
+        });
+
+    }
+
+    /**
+     * Display Progress bar while registering
+     */
+    private void displayLoader() {
+        pDialog = new ProgressDialog(RegisterActivity.this);
+        pDialog.setMessage("Signing Up.. Please wait...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+    }
+
+    /**
+     * Launch Dashboard Activity on Successful Sign Up
+     */
+    private void loadDashboard() {
+        Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+        startActivity(i);
+        finish();
+
+    }
+
+    private void registerUser() {
+        displayLoader();
+        JSONObject request = new JSONObject();
+        try {
+            //Populate the request parameters
+            request.put(KEY_USERNAME, username);
+            request.put(KEY_PASSWORD, password);
+            request.put(KEY_EMAIL, email);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsArrayRequest = new JsonObjectRequest
+                (Request.Method.POST, register_url, request, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        pDialog.dismiss();
+                        try {
+                            //Check if user got registered successfully
+                            if (response.getInt(KEY_STATUS) == 0) {
+                                //Set the user session
+                                session.loginUser(username,email);
+                                loadDashboard();
+
+                            }else if(response.getInt(KEY_STATUS) == 1){
+                                //Display error message if username is already existsing
+                                etUsername.setError("Username already taken!");
+                                etUsername.requestFocus();
+
+                            }else{
+                                Toast.makeText(getApplicationContext(),
+                                        response.getString(KEY_MESSAGE), Toast.LENGTH_SHORT).show();
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pDialog.dismiss();
+
+                        //Display error message whenever an error occurs
+                        Toast.makeText(getApplicationContext(),
+                                error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsArrayRequest);
+    }
+
+    /**
+     * Validates inputs and shows error if any
+     * @return
+     */
+    private boolean validateInputs() {
+        if (KEY_EMPTY.equals(email)) {
+            etEmail.setError("Full Name cannot be empty");
+            etEmail.requestFocus();
+            return false;
+
+        }
+        if (KEY_EMPTY.equals(username)) {
+            etUsername.setError("Username cannot be empty");
+            etUsername.requestFocus();
+            return false;
+        }
+        if (KEY_EMPTY.equals(password)) {
+            etPassword.setError("Password cannot be empty");
+            etPassword.requestFocus();
+            return false;
+        }
+
+        if (KEY_EMPTY.equals(confirmPassword)) {
+            etConfirmPassword.setError("Confirm Password cannot be empty");
+            etConfirmPassword.requestFocus();
+            return false;
+        }
+        if (!password.equals(confirmPassword)) {
+            etConfirmPassword.setError("Password and Confirm Password does not match");
+            etConfirmPassword.requestFocus();
+            return false;
+        }
+
+        return true;
+    }
+}
